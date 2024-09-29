2471a1b213754404f06504a052cf3cbbfa1a96aa
Kalpesh More <90396306+kalpeshmore1234@users.noreply.github.com>
2024-09-29 10:11:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import JobInfo from "./JobInfo";
+import BtnContainer from "./BtnContainer";
+
+const url = "https://www.course-api.com/react-tabs-project";
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [currentItem, setCurrentItem] = useState(0)
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+
+  //currentItem
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+
+  return (
+    <section className="jobs-center">
+       {/* button container */}
+       <BtnContainer jobs={jobs} currentItem={currentItem} setCurrentItem={setCurrentItem}/>
+      {/* job info */}
+      <JobInfo jobs={jobs} currentItem={currentItem} />
+    </section>
+  );
+};
+export default App;
---
+++ src/Duties.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {FaAngleDoubleRight} from 'react-icons/fa'
+import {v4 as uuidv4} from 'uuid'
+
+const Duties = ({duties}) => {
+
+    const id = uuidv4()
+    console.log(id)
+
+  return (
+    <div>
+        {duties.map((duty, index) => {
+             
+             const id = uuidv4()
+            console.log(id) 
+
+            console.log(duty)
+            return <div key={index} className='job-desc'>
+                <FaAngleDoubleRight className='job-icon' />
+                <p>{duty}</p>
+            </div>
+        })}
+    </div>
+  )
+}
+
+export default Duties
